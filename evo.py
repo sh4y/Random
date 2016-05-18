0e1288cf31097d280dfc937c25d7d03275b6ddb6
sh4y <akshay.hariharan@mail.utoronto.ca>
2016-05-18 19:44:50 -0400

    Evolution of Text

---
+++ evo.py
@@ -0,0 +1,22 @@
+from random import choice
+import string
+
+def main():
+	text = raw_input("Enter your text: ")
+	tarr = [character for character in text]
+	rand = []
+	for i in range(0, len(tarr)):
+		rand.append(choice(string.ascii_letters))
+	iterations = 0
+	while rand != tarr:
+		iterations += 1
+		for ch in range(0, len(tarr)):
+			if tarr[ch] == ' ':
+				rand[ch] = ' '
+			if tarr[ch] != rand[ch]:
+				rand[ch] = choice(string.ascii_letters)
+		print(''.join(rand))
+	print("Iterations taken: ", iterations)
+
+main()
+
