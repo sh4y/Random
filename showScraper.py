a961560dea78b9823b03399ebb420543725ab487
sh4y <akshay.hariharan@mail.utoronto.ca>
2016-05-23 15:32:17 -0400

    Added new show support
    To print all links of a show, paste the show URL into a text file named 'new.txt' and via the UNIX command line run:
    $ python showScraper.py new.txt
    
    Otherwise, your list of shows can be in any text file (default is 'shows.txt'), new episode tracker can be ran by typing
    $ python showScraper.py
    or
    $ python showScraper.py yourfilename.txt

--- showScraper.py
+++ showScraper.py
@@ -1,12 +1,12 @@
 import urllib2
 import re
+import sys
 
-shows = []
-
-f = open('shows.txt', 'r')
-for line in f:
-    shows.append(line.strip('\n'))
+name = 'shows.txt'
+if len(sys.argv) > 1:
+    name = str(sys.argv[1])
     
+shows = []
 def extractName(string):
     return re.findall('<span class="tv_episode_name"> - (.*)<\/span>', string)[0]
 
@@ -23,6 +23,19 @@ def greatestIndex(lst):
             m = i
     return m
 
+def printList(lst):
+    for i in lst:
+        print(i)
+
+if name == 'new.txt':
+    PRINT_LINKS = True
+else:
+    PRINT_LINKS = False
+    
+f = open(name, 'r')
+for line in f:
+    shows.append(line.strip('\n'))
+    
 for url in shows:
     print('--------------------------------------------------------------------------------------------')
     page = urllib2.urlopen(url).read()
@@ -35,6 +48,7 @@ for url in shows:
     newest = greatestIndex(dates)
     i = 0
     for item in re.findall('<div class="tv_episode_item".*<\/span>', page):
+        links.append(extractLink(item))
         episodes.append(item)
         i += 1
         if i-1 == newest:
@@ -42,4 +56,5 @@ for url in shows:
             print("Episode number: " + extractLatestEpisode(item))
             print(extractLink(item))
             print("Date: " + dates[newest])
-        
+    if PRINT_LINKS:
+        printList(links)
